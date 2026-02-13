b01e8cc3cf660975b5c048ccf53c51426101ba50
Rodrigo Núñez <rodrigo.nc2008@gmail.com>
2026-02-13 11:57:30 +0100

    Cards

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,21 +3,38 @@ import { defineCollection, z } from "astro:content";
 const landing = defineCollection({
   type: "data",
   schema: z.object({
-    cardSections: z.array(
-    z.object({
-        id: z.string(),
-        title: z.string(),
-        items: z.array(
-        z.object({
-            title: z.string(),
-            body: z.string(),
+    fullWidthCard: z.object({
+      title: z.string(),
+      headline: z.string(),
+      body: z.string(),
+      icons: z
+        .array(
+          z.object({
             icon: z.string(),
-            iconAlt: z.string().optional().default("")
-        })
+            iconAlt: z.string().optional().default(""),
+          })
         )
-    })
-    ).default([])
+        .default([]),
+    }),
 
+    threeColumnsCards: z
+      .array(
+        z.object({
+          id: z.string(),
+          title: z.string(),
+          items: z
+            .array(
+              z.object({
+                title: z.string(),
+                body: z.string(),
+                icon: z.string(),
+                iconAlt: z.string().optional().default(""),
+              })
+            )
+            .default([]),
+        })
+      )
+      .default([]),
   }),
 });
 
